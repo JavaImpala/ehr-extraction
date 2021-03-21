6f2e368ad82cbe1a1310ed59dd19de69664a0a11
JavaImpala <torbjorn@torsvik.net>
2021-03-21 21:34:42 +0100

    skilt ut filereading i main

--- ehr-extraction/src/main/java/Main.java
+++ ehr-extraction/src/main/java/Main.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import org.apache.commons.lang3.mutable.MutableObject;
@@ -14,60 +15,34 @@ import reports.ProfilCarePlanParser;
 import util.pageProcessor.PageParserManager;
 
 public class Main {
-	public static void main( String[] args )  
-    {  
-		try {
-			String location="/media/tor003/6887-88BA/ABB/forskninguttrekkABB050321full";
-			File folder=new File(location);
-			
-			//number-date(dd.mm.yyyy)-Skrevet av: [name]-Rapport-Rapportert dato: date(dd.mm.yyy
-			
-			int noteCounter=0;
+	public static void main(String[] args ){  
 		
-			List<Supplier<PageParserManager>> pageParsers=new ArrayList<>();
-			
-			
-			
-			//lager reportMaker
-			
-			
-			pageParsers.add(()->ProfilCarePlanParser.create());
-			
-			MutableObject<Optional<PageParserManager>> currentManager=new MutableObject<>(Optional.empty());	
-			MutableObject<Runnable> closeCurrentResource=new MutableObject<>(()->{});
-			
-			for(File fileEntry:folder.listFiles()) {
-				
-				
-				String type=Files.probeContentType(fileEntry.toPath());
-				
-				if(type.equals("text/plain")) {
-					
-						
-					//FileReader reader=new FileReader(fileEntry);
-					
-					IOSupplier<Iterator<String>> getLines=()->{
-						
-						FileReader reader=new FileReader(fileEntry);
-						
-						closeCurrentResource.setValue(()->reader.close());
-						
-						return new BufferedReader(reader).lines().iterator();
-						
-					};
-					
-					//tries to see if current parser can take this page as well
+		String location="/media/tor003/6887-88BA/ABB/forskninguttrekkABB050321full";
+		
+		//number-date(dd.mm.yyyy)-Skrevet av: [name]-Rapport-Rapportert dato: date(dd.mm.yyy
+		
+		List<Supplier<PageParserManager>> pageParsers=new ArrayList<>();
+		pageParsers.add(()->ProfilCarePlanParser.create());
+		
+		//lager reportMaker
+		MutableObject<Optional<PageParserManager>> currentManager=new MutableObject<>(Optional.empty());	
+		
+		new PageLineSupplier(
+				location,
+				pageLineIteratorSupplier->{
 					if(currentManager.getValue().isPresent()) {
+						//Iterator<String> 
+						
 						boolean success=currentManager
 								.getValue()
 								.get()
 								.getPageParser()
-								.tryToProccessPage(getLines);
+								.tryToProccessPage(pageLineIteratorSupplier);
 						
 						if(success) {
 							
 							//if success, skip to next page
-							continue;
+							return;
 						}else {
 							currentManager.setValue(Optional.empty());
 						}
@@ -83,7 +58,7 @@ public class Main {
 						
 						boolean success=instance
 								.getPageParser()
-								.tryToProccessPage(getLines);
+								.tryToProccessPage(pageLineIteratorSupplier);
 						
 						if(success) {
 							System.out.println("initierer ny parser");
@@ -91,15 +66,69 @@ public class Main {
 							break;
 						}
 					}
-				}
-			}
-				
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
+				});
     }  
 	
-	interface IOSupplier<E> {
+	private static class PageLineSupplier{
+		
+		
+		private PageLineSupplier(String location,Consumer<Supplier<Iterator<String>>> listener) {
+			
+			try {
+				File folder=new File(location);
+				
+				//number-date(dd.mm.yyyy)-Skrevet av: [name]-Rapport-Rapportert dato: date(dd.mm.yyy
+				
+				MutableObject<IORunnable> closeCurrentResource=new MutableObject<>(()->{});
+				
+				for(File fileEntry:folder.listFiles()) {
+					
+					String type=Files.probeContentType(fileEntry.toPath());
+					
+					if(type.equals("text/plain")) {
+						
+							
+						FileReader reader=new FileReader(fileEntry);
+						
+						
+						Supplier<Iterator<String>> getLines=()->{
+							try {
+								
+								closeCurrentResource.getValue().run();
+								
+								
+								BufferedReader br=new BufferedReader(reader);
+								
+								closeCurrentResource.setValue(()->br.close());
+								
+								Iterator<String> iterator=br.lines().iterator();
+								
+								return iterator;
+							
+							} catch (Exception e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							
+							return new ArrayList<String>().iterator();
+						};
+						
+						listener.accept(getLines);
+						
+						reader.close();
+					}
+				}
+			}catch(Exception e) {
+				
+			}
+		}
+	}
+	
+	private interface IOSupplier<E> {
 		public E get() throws Exception;
+	}
+	
+	private interface IORunnable {
+		public void run() throws Exception;
 	}
 }
